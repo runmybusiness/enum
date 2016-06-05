b8b5ef51fa7594bf6c9d851c87251c720152cd50
Tom Schlick <tom@tomschlick.com>
2016-06-05 15:17:37 -0400

    Applied fixes from StyleCI

--- tests/DummyClass.php
+++ tests/DummyClass.php
@@ -5,16 +5,14 @@ namespace RunMyBusiness\Enum\Tests;
 use RunMyBusiness\Enum\Enum;
 
 /**
- * Class DummyClass
- * @package RunMyBusiness\Enum\Tests
+ * Class DummyClass.
  */
 class DummyClass
 {
     use Enum;
 
-    const STATUS_NEW      = 1;
-    const STATUS_READ     = 2;
+    const STATUS_NEW = 1;
+    const STATUS_READ = 2;
     const STATUS_ARCHIVED = 3;
-    const STATUS_DELETED  = 4;
-
+    const STATUS_DELETED = 4;
 }
